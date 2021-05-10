55afe3fbeeed5b6586d0dd311a9fa1fa76eaa745
Clement <clement.delbarre@ensweet.fr>
2021-05-10 15:58:33 +0200

    upload form

--- src/Screens/UploadPage/UploadPage.jsx
+++ src/Screens/UploadPage/UploadPage.jsx
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import { userService } from '@/Services';
-import {Button, Form}from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function UploadPage (props){
     const [users, setUsers] = useState(null)
@@ -13,10 +11,32 @@ function UploadPage (props){
         return (
             <div>
                 <h1>Upload d'image</h1>
-                <p>This page can only be accessed by administrators.</p>
-                <Form>
-                    
-                </Form>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="inputName">Nom</label>
+                        <input type="text" className="form-control" id="inputName"
+                               placeholder="Nom de l'image"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputDescription">Description</label>
+                        <input type="text" className="form-control" id="inputDescription"
+                               placeholder="Description de l'image"/>
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="imageFile">Fichier image</label>
+                        <input type="file" className="form-control" id="imageFile"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputCopyright">Copyright</label>
+                        <input type="text" className="form-control" id="inputCopyright"
+                               placeholder="Copyright de l'image"/>
+                    </div>
+                    <div className="form-check">
+                        <input type="checkbox" className="form-check-input" id="isPublished"/>
+                            <label className="form-check-label" htmlFor="isPublished">Publier l'image</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Envoyer</button>
+                </form>
             </div>
         );
 }
