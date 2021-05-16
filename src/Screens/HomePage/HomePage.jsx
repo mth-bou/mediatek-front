cbaecf55a00a2c4e36ddc7c81caa48ace119fc0c
Clement <clement.delbarre@esweet.fr>
2021-05-16 14:13:32 +0200

    home page

--- src/Screens/HomePage/HomePage.jsx
+++ src/Screens/HomePage/HomePage.jsx
@@ -4,19 +4,26 @@ import {userService, authenticationService} from '@/Services';
 import CardImage from "@/Components/cardImage";
 import axios from "axios";
 import config from "../../../config";
+import Button from "react-bootstrap/Button";
 
 function HomePage() {
 
     const [currentUser, setCurrentUser] = useState(authenticationService.currentUserValue)
     const [userFromApi, setUserFromApi] = useState(null)
-    const [imageList, setImageList] = useState(null)
+    const [imageListSorted, setImageListSorted] = useState([])
 
     useEffect(() => {
         userService.getById(currentUser.id).then(userFromApi => setUserFromApi(userFromApi));
         try {
             let response = axios.get(`${config.apiUrl}/images`)
                 .then(resp => {
-                    setImageList(resp.data)
+                    resp.data.map(item => {
+                        item.datePublished = Date.parse(item.datePublished)
+                        imageListSorted.push(item)
+                        imageListSorted.sort(function (a, b) {
+                            return new Date(b.datePublished) - new Date(a.datePublished)
+                        })
+                    })
                 })
         } catch (error) {
             console.log(error)
@@ -26,6 +33,7 @@ function HomePage() {
     return (
         <div>
             <div style={{background: '#E9ECEF', textAlign: 'center'}}>
+                <Button onClick={() => console.log(imageListSorted)}></Button>
                 <h1 className="text-center">Bienvenue sur Mediatek</h1>
                 <p>Une application qui vous permet de gérer une banque d'images comme vous le souhaitez</p>
                 <div>
@@ -37,8 +45,8 @@ function HomePage() {
             </div>
 
             <div className="d-inline-flex flex-wrap mt-4">
-                {imageList && currentUser.role === Role.User &&
-                imageList.slice(0, 10).map(image =>
+                {imageListSorted && currentUser.role === Role.User &&
+                imageListSorted.map(image =>
                     image.visibility &&
                     <div className="col-4" key={image.id}>
                         <CardImage image={image}/>
@@ -47,8 +55,8 @@ function HomePage() {
                 }
 
                 {
-                    imageList && currentUser.role === Role.Admin &&
-                    imageList.map(image =>
+                    imageListSorted && currentUser.role === Role.Admin &&
+                    imageListSorted.map(image =>
                         <div className="col-4" key={image.id}>
                             <CardImage image={image}/>
                         </div>
